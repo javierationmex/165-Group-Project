0a94f1708ead546174ceb6a3eeb3b4fe0bcd7394
Max <maxkondras@gmail.com>
2015-04-20 23:21:18 -0700

    fix

--- src/gameengine/player/MovePlayerRightAction.java
+++ src/gameengine/player/MovePlayerRightAction.java
@@ -1,6 +1,7 @@
 package gameengine.player;
 
 import graphicslib3D.Matrix3D;
+import graphicslib3D.Point3D;
 import graphicslib3D.Vector3D;
 import net.java.games.input.Event;
 import networking.Client;
@@ -34,9 +35,9 @@ public class MovePlayerRightAction extends BaseAbstractInputAction {
         Vector3D dir = new Vector3D(1,0,0);
         if (avatar.getWorldBound().contains(terrainPoint)) dir = new Vector3D(-1, 0, 0);
         dir = dir.mult(rot);
-        dir.scale((double) (speed * time));
+        dir.scale((double)(speed * time));
         avatar.translate((float) dir.getX(), (float) dir.getY(), (float) dir.getZ());
-
+        sendUpdateLocationPacket(client, avatar.getLocalTranslation(), avatar.getLocalScale(), avatar.getLocalRotation());
     }
 
 }
