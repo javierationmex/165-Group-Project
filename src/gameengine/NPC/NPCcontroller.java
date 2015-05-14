2a8cecb1a8c1315c62336bd05afb6ea07ee3ab55
arash <arashparnia@gmail.com>
2015-05-13 20:57:12 -0700

    NPC start

---
+++ src/gameengine/NPC/NPCcontroller.java
@@ -0,0 +1,16 @@
+package gameengine.NPC;
+
+/**
+ * Created by arash on 5/13/2015.
+ */
+public class NPCcontroller {
+
+    private NPC[] NPClist = new NPC[5];
+
+    public void updateNPCs() {
+//            for (int i=0; i<numNPCs; i++) {
+//                NPClist[i].updateLocation();
+//            }
+    }
+
+}
